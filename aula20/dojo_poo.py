7a1532f06f823a04292641201b8fb2009c015c12
Mario Sergio <sergio.mario_q@hotmail.com>
2019-02-28 22:01:33 -0300

    aula21

--- aula20/dojo_poo.py
+++ aula20/dojo_poo.py
@@ -12,13 +12,40 @@ class Cliente:
         self.saldo = saldo
         
 class CaixaEletronico:
-    def __init__(self, cedulas):
-        self.cedulas = cedulas
+    def __init__(self):
+        self.cedulas = [100, 50, 20, 10]
 
     def saque(self, valor_saque, cliente):
         if valor_saque <= cliente.saldo:
             return True
         return False
+
+    def valida_saque(self, valor_saque): 
+        if valor_saque % 10 == 0:
+            return True 
+        elif valor_saque == float("inf"):
+            return True
+        return False 
+
+    def valida_cedulas(self, valor_saque):
+        if not self.valida_saque(valor_saque):
+            return False
+
+        if valor_saque == float("inf"):
+            return {100: float("inf"),
+                    50: float("inf"),
+                    20: float("inf"),
+                    10: float("inf")} 
+
+        quantidade_cedulas = {100:0, 50:0, 20:0, 10:0}
+        for cedula in self.cedulas:
+            quantidade_cedulas[cedula] = valor_saque // cedula
+
+            if valor_saque % cedula == 0:
+                return quantidade_cedulas;
+            else:
+                valor_saque = valor_saque % cedula
+
 
 class DojoPOOMethods(unittest.TestCase):
 
@@ -41,7 +68,26 @@ class DojoPOOMethods(unittest.TestCase):
     def test_saque_infi_false(self):
         self.assertFalse(self.caixa_eletronico.saque(float("inf"),self.everton))
 
+    def test_valida_saque(self):
+        self.assertTrue(self.caixa_eletronico.valida_saque(300))
 
+    def test_valida_saque_invalido(self):
+        self.assertFalse(self.caixa_eletronico.valida_saque(57))
+       
+    def test_valida_saque_infinito(self):
+        self.assertTrue(self.caixa_eletronico.valida_saque(float("inf")))
+
+    def test_valida_cedulas_130(self):
+        self.assertEqual(self.caixa_eletronico.valida_cedulas(130), {20: 1, 50: 0, 100: 1, 10: 1})
+
+    def test_valida_cedulas_103030(self):
+        self.assertEqual(self.caixa_eletronico.valida_cedulas(103030), {20: 1, 50: 0, 100: 1030, 10: 1})
+
+    def test_valida_cedulas_80(self):
+        self.assertEqual(self.caixa_eletronico.valida_cedulas(80), {20: 1, 50: 1, 100: 0, 10: 1})
+
+    def test_valida_cedulas_infinito(self):
+        self.assertEqual(self.caixa_eletronico.valida_cedulas(float("inf")), {100: float("inf"), 50: float("inf"), 20: float("inf"), 10: float("inf")})
 
 if __name__ == '__main__':
     unittest.main()
