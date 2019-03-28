6c222e778deed06416603eb2ef6d774a06c67501
Mario Sergio <sergio.mario_q@hotmail.com>
2019-03-28 20:03:31 -0300

    novas views

--- aula17/nutricao/nutricao/api/serializers.py
+++ aula17/nutricao/nutricao/api/serializers.py
@@ -4,12 +4,22 @@ from paciente.models import Paciente, Dieta
 
 
 class DietaSerializer(serializers.ModelSerializer):
-    paciente = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Dieta
-        fields = ('paciente', 'plano_alimentar', 'periodo', 'paciente')
+        fields = ('plano_alimentar', 'periodo')
 
 class PacienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Paciente
+        fields = ('nome', 'idade', 'telefone', 'profissao', 'email')
+
+class DietaPacientesSerializer(serializers.ModelSerializer):
+    paciente = PacienteSerializer(many=True, read_only=True)
+    class Meta:
+        model = Dieta
+        fields = ('paciente', 'plano_alimentar', 'periodo')
+
+class PacienteDietasSerializer(serializers.ModelSerializer):
     dieta = DietaSerializer(many=True, read_only=True)
     class Meta:
         model = Paciente
