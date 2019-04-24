49b926d307fe7a60f20365c32a87cd4e83934742
Mario Sergio <mariopekenossalro@gmail.com>
2019-04-24 11:13:45 -0300

    aula 35

--- aula34/unicef.py
+++ aula34/unicef.py
@@ -1,4 +1,8 @@
+import ast
+import csv
+import sqlite3
 import xlrd
+
 import pandas as pd
 
 
@@ -7,9 +11,31 @@ sheet = workbook.sheet_by_index(0)
 
 lista = []
 
-for row in range(12, sheet.nrows):
+for row in range(12, sheet.nrows-5):
     lista.append(sheet.row_values(row))
 cols = sheet.row_values(11)
 
 df = pd.DataFrame(data=lista, columns=cols)
-print(df)
+csv_file = df.to_csv('data.csv')
+
+
+list_columns = df.columns.to_list()
+list_columns.insert(0, 'id')
+
+for index, x in enumerate(list_columns):
+    list_columns[index] = str(x)
+
+con = sqlite3.connect("db.sqlite3")
+cur = con.cursor()
+cur.execute("CREATE TABLE mortality_rate_countrie {}".format(tuple(list_columns)))
+
+with open('data.csv','rb') as data:
+    lines = data.readlines()
+
+    for line in lines[1:]:
+        line = str(line).split(',')
+        cur.execute("INSERT INTO mortality_rate_countrie VALUES {};".format(tuple(line)))
+
+con.commit()
+con.close()
+
