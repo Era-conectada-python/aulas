5864fcb5059650623d9b345a4e957941ce5dc830
Mario Sergio <mariopekenossalro@gmail.com>
2019-04-30 22:04:17 -0300

    refactory unicef data

--- aula34/unicef.py
+++ aula34/unicef.py
@@ -1,4 +1,5 @@
 import ast
+import click
 import csv
 import sqlite3
 import xlrd
@@ -6,36 +7,71 @@ import xlrd
 import pandas as pd
 
 
-workbook = xlrd.open_workbook("IMR_mortality_rate_2018.xlsx")
-sheet = workbook.sheet_by_index(0)
-
-lista = []
-
-for row in range(12, sheet.nrows-5):
-    lista.append(sheet.row_values(row))
-cols = sheet.row_values(11)
-
-df = pd.DataFrame(data=lista, columns=cols)
-csv_file = df.to_csv('data.csv')
+def make_table(name, columns, rows):
+    con = sqlite3.connect("db.sqlite3")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE {} {}".format(name, tuple(columns)))
+    for row in rows:
+        cur.execute("INSERT INTO {} {} VALUES {};".format(name, tuple(columns), tuple(row)))
+    con.commit()
+    con.close()
 
 
-list_columns = df.columns.to_list()
-list_columns.insert(0, 'id')
+def generate_coutry_data(columns, data):
+    cols_coutries = columns[:2]
+    coutries_values = [value[:2] for value in data]
+    return cols_coutries, coutries_values
 
-for index, x in enumerate(list_columns):
-    list_columns[index] = str(x)
 
-con = sqlite3.connect("db.sqlite3")
-cur = con.cursor()
-cur.execute("CREATE TABLE mortality_rate_countrie {}".format(tuple(list_columns)))
+def generate_mortality_data(columns, data):
+    cols_years = columns[2:]
+    cols_mortality = [columns[0], 'median', 'year']
+    mortality_values_list = [[[value[0], median, cols_years[index]]
+                        for index, median in enumerate(value[2:])]
+                        for value in data[2:]]
+    mortality_values = []
+    for values in mortality_values_list:
+        for element in values:
+            mortality_values.append(element)
 
-with open('data.csv','rb') as data:
-    lines = data.readlines()
+    return cols_mortality, mortality_values
 
-    for line in lines[1:]:
-        line = str(line).split(',')
-        cur.execute("INSERT INTO mortality_rate_countrie VALUES {};".format(tuple(line)))
 
-con.commit()
-con.close()
+def read_file(file):
+    workbook = xlrd.open_workbook(file)
+    sheet = workbook.sheet_by_index(0)
+    data = []
+    for row in range(12, sheet.nrows-5):
+        row_data = sheet.row_values(row)
+        if not 'Lower' in row_data and not 'Upper' in row_data:
+            del(row_data[2])
+            data.append(row_data)
+
+    columns_data = sheet.row_values(11)
+    del(columns_data[2])
+    for index, value in enumerate(columns_data):
+        columns_data[index] = str(value).lower().replace(' ', '_').replace('.5', '')
+
+    return columns_data, data
+
+@click.command()
+@click.option('--filename', prompt='filename with data mortality',
+             help='File with all data mortality by country in xlsx extension.')
+def main(filename):
+    cols, data_list = read_file(filename)
+
+    cols_coutries, coutries_values = generate_coutry_data(cols, data_list)
+    make_table(name='country', columns=cols_coutries, rows=coutries_values)
+
+    cols_mortality, mortality_values = generate_mortality_data(cols, data_list)
+    make_table(name='mortality', columns=cols_mortality, rows=mortality_values)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except(FileNotFoundError):
+        click.echo("Invalid file!")
+    except(xlrd.biffh.XLRDError):
+        click.echo("Invalid file format!")
 
